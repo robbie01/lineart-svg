062554e12cbac35e3f66323186e58978807b0d2e
Robert Langer <robbie0630dev@gmail.com>
2018-04-21 17:24:40 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import LineArt from './LineArt'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      radius: 200,
+      npoints: 24
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <LineArt radius={this.state.radius} npoints={this.state.npoints} /><br />
+        <label>
+          Radius
+          <input type="number" value={this.state.radius}
+            onChange={e => this.setState({ radius: parseInt(e.target.value, 10) })} />
+        </label><br />
+        <label>
+          Points
+          <input type="number" value={this.state.npoints}
+            onChange={e => this.setState({ npoints: parseInt(e.target.value, 10) })} />
+        </label>
+      </div>
+    )
+  }
+}
+
+export default App
